36f3a70703a33151c849f3ebfee90920c7f797ea
ArcaneBlackwood <redstonemaster4952@gmail.com>
2026-09-12 12:26:59 +1200

    Better commands :3

--- commands/runServer.sh
+++ commands/runServer.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-"$(dirname -- "${BASH_SOURCE[0]}")/../gradlew" runServer --console=plain 2>&1 | less -RS +F
+killtree() {
+    local pid=$1
+    for child in $(ps -o pid= --ppid "$pid"); do
+        killtree "$child"
+    done
+    kill -TERM "$pid" 2>/dev/null
+}
+
+coproc GRADLE { "$(dirname -- "${BASH_SOURCE[0]}")/../gradlew" runServer --console=plain 2>&1; }
+less -RS +F <&"${GRADLE[0]}"
+killtree "$GRADLE_PID"
